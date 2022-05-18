ad4d4ef1e04c724d31bd99262bde1faf006c7f82
Kyle Wisecarver <102608427+WisecarverWorks@users.noreply.github.com>
2022-05-18 01:56:21 -0500

    Registered project app and rest framework in project settings installed apps section.

--- products_project/products/models.py
+++ products_project/products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
 
     
--- products_project/products_project/settings.py
+++ products_project/products_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'products',
 ]
 
@@ -120,3 +121,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try: 
+    from products_project.local_settings import*
+except ImportError:
+    pass
